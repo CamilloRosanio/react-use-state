62aef5ba6fde8630fac8c9d52926ca36a655d74c
MyUsername <rosanio.c@gmail.com>
2024-12-02 16:05:45 +0100

    ripristino struttura di base + map TAGS

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -6,11 +6,19 @@ function Tags() {
 
     return (
         <>
-            <ul className={styles.tagList}>
-                {Languages.map((item, index) => (
-                    <li className={styles.tagLabel + ' ' + item.title} key={index}>{item.title}</li>
-                ))}
-            </ul>
+            <section className='tagsContainer'>
+                <ul className={styles.tagList}>
+                    {Languages.map((item, index) => (
+                        <li className={styles.tagLabel + ' ' + item.title} key={index}>{item.title}</li>
+                    ))}
+                </ul>
+
+                <div className="tagDetail">
+                    <h3>{Languages[0].title}</h3>
+                    <p>{Languages[0].description}</p>
+                </div>
+
+            </section>
         </>
     )
 }
